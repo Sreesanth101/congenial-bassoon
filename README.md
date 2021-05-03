7d53bd7ee0770be9c1839d68662a207e9644b7fb
Amazeryogo <suvid.datta@gmail.com>
2021-05-03 14:17:43 +0530

    this is much better

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# congenial-bassoon
-## What is the project about?
-The project is to store and query data in text/.json files instead of Databases like Posgres and MongoDB
-
-## Its not a PyPi approved library yet as it is still under development, but you can test it if you like.
-
-
-## What do you mean by "minors" in your code?
-If you want a function/user that only fetches data but does not insert any data, you can create a minor user, there is only one major user (for now), the user that you make while connecting to your database
+# 
--- __init__.py
+++ __init__.py
@@ -1,1 +1,61 @@
-from backbone import *
+import json
+
+class Database:
+    def __init__(self,path,username,password):
+        self.path = path
+        self.username = username
+        self.password= password
+        self.connected = False
+    def connect(self,username,password):
+        if username == self.username and password == self.password:
+            with open(self.path,'r') as f:
+                f.close()
+            self.connected = True
+        else:
+            raise PermissionError('Username and password are not correct')
+    def close(self):
+        if self.connected == True:
+            self.connected = False
+        else:
+            raise IOError('connection already closed')
+
+    def add(self,dictionary):
+        if self.connected == True:
+            if type(dictionary) == dict:
+                with open(self.path,'ab+') as f:
+                    f.seek(0, 2)
+                    if f.tell() == 0:  #
+                        f.write(json.dumps([dictionary]).encode())
+                    else:
+                        f.seek(-1, 2)
+                        f.truncate()
+                        f.write(' , '.encode())
+                        f.write('\n'.encode())
+                        f.write(json.dumps(dictionary).encode())
+                        f.write(']'.encode())
+            else:
+                raise TypeError(dictionary,'Is not a dictionary to dump in json')
+        else:
+            raise ConnectionError('Not connected')
+
+    def get_unfiltered(self):
+        if self.connected == True:
+            with open(self.path,'r') as f:
+                x = f.read()
+                x = json.loads(x)
+                return x
+        else:
+            raise ConnectionError('Not connected')
+
+    def get_json(self):
+        if self.connected == True:
+            with open(self.path,'r') as f:
+                x = f.read()
+                x = json.loads(x) # just to make sure
+                x = json.dumps(x)
+                return x
+        else:
+            raise ConnectionError('Not connected')
+
+    def version(self):
+        return 'congenial-bassoon v0.0-beta'
